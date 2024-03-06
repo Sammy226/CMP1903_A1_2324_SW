a2f81854e08a575768c0bcc5282eefde1ac6085a
Sammy226 <samuelcmwright@live.co.uk>
2024-03-06 23:55:10 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -12,17 +12,22 @@ namespace CMP1903_A1_2324
         Die DiceTest = new Die();
         Game GameTest = new Game();
 
+
         public void Test()
         {
             if (DiceTest.Roll() != 0)
             {
-                Debug.Assert((1 > DiceTest.Roll()) && (6 < DiceTest.Roll())); // this prevents the dice from rolling higher then a 6 or lower then 1
-                Console.WriteLine("Dice value exceeds the range");
+                Debug.Assert((1 <= DiceTest.Roll()) || (6 >= DiceTest.Roll())); // this prevents the dice from rolling higher then a 6 or lower then 1
+                Console.WriteLine(" ");
             }
             else if (GameTest.RollDice() != 0)
             {
-                Debug.Assert((3 > GameTest.RollDice()) && (18 < GameTest.RollDice())); // this prevents the total dice rolls from going higher then the max (18) and lower then the min (3)
-                Console.WriteLine("Total dice value exceeds the range");
+                Debug.Assert((3 <= GameTest.RollDice()) || (18 >= GameTest.RollDice())); // this prevents the total dice rolls from going higher then the max (18) and lower then the min (3)
+                Console.WriteLine(" ");
+            }
+            else
+            {
+                Console.WriteLine("dice value exceeds range");
             }
         }
     }
