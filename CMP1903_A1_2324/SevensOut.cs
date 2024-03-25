9f59f4607b23513c37d6385b77b284521d6e835e
Sammy226 <samuelcmwright@live.co.uk>
2024-03-25 14:06:42 +0000

    started sevens out

--- CMP1903_A1_2324/SevensOut.cs
+++ CMP1903_A1_2324/SevensOut.cs
@@ -8,5 +8,40 @@ namespace CMP1903_A1_2324
 {
     internal class SevensOut
     {
+        public Die dice1 = new Die();
+        public Die dice2 = new Die();
+
+        int TotalScore1 = 0;
+
+        public int PlaySevens()
+        {
+            bool SevensLoop1 = true;
+            while (SevensLoop1 == true)
+            {
+                Console.ReadKey();
+                int diceA = dice1.Roll();
+                int diceB = dice2.Roll();
+                if (diceA == diceB)
+                {
+                    int DiceScore = diceA + diceB * 2;
+                       TotalScore1 = TotalScore1 + DiceScore;
+                }
+                else if (diceA + diceB == 7)
+                {
+                    SevensLoop1 = false;
+                }
+                else
+                {
+                    TotalScore1 = diceA + diceB;
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine("Dice 1 rolled: " + diceA);
+                Console.WriteLine("Dice 2 rolled: " + diceB);
+                Console.WriteLine("The total adds up to: " + TotalScore1);
+                Console.WriteLine(" ");
+                Console.WriteLine("Press anything to roll again");
+            }
+
+        }
     }
 }
