468a08134fc1a8697e910068a43289f5ced2b84a
Sammy226 <samuelcmwright@live.co.uk>
2024-03-28 16:10:11 +0000

    Update ThreeOrMore.cs

--- CMP1903_A1_2324/ThreeOrMore.cs
+++ CMP1903_A1_2324/ThreeOrMore.cs
@@ -17,7 +17,42 @@ namespace CMP1903_A1_2324
         
         public void PlayThreeOrMore()
         {
-
+            Console.WriteLine("Game Started");
+            Console.WriteLine("Player 1 starts");
+            Console.WriteLine("Press anything to roll");
+            Console.WriteLine(" ");
+            Console.ReadKey();
+            int diceA = dice1.Roll();
+            int diceB = dice2.Roll();
+            int diceC = dice3.Roll();
+            int diceD = dice4.Roll();
+            int diceE = dice5.Roll();
+            Console.WriteLine("Dice 1: " + diceA);
+            Console.WriteLine("Dice 2: " + diceB);
+            Console.WriteLine("Dice 3: " + diceC);
+            Console.WriteLine("Dice 4: " + diceD);
+            Console.WriteLine("Dice 5: " + diceE);
+            Console.WriteLine(" ");
+            Console.WriteLine("Please select the dice you wish to re-roll.");
+            Console.WriteLine("Example: '1'");
+            Console.WriteLine("You will get to select more dice after if you want");
+            Console.WriteLine("If you have selected all the dice you wish to reroll type 'done'");
+            bool ChoiceLoop = true;
+            List<int> RollingDice = new List<int>();
+            while (ChoiceLoop == true)
+            {
+                
+                string DiceChoice = Console.ReadLine();
+                if (DiceChoice == "done")
+                {
+                    ChoiceLoop = false;
+                }
+                else
+                {
+                    int NewDiceChoice = int.Parse(DiceChoice);
+                    RollingDice.Add(NewDiceChoice);
+                }
+            }    
         }
     }
 }
